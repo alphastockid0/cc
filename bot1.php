4811e30e7691088b4c5d9e98d231bc008315b37f
alphastockid0 <62582615+alphastockid0@users.noreply.github.com>
2020-03-28 16:00:24 +0700

    Update bot1.php

--- bot1.php
+++ bot1.php
@@ -3,7 +3,7 @@ ulang:
 $uuid = 'd' . rand(111,999) . 'ab7c-' . rand(1000,9999) . '-' . rand(1000,9999) . '-' . rand(1000,9999) . '-' . rand(1000,9999) .'fab' . rand(11111,99999);
 
 $areaCode = 62;
-echo "Country Number : $areaCode"; 
+// echo "Country Number : $areaCode"; 
 
 echo "Enter Phone Number : $areaCode"; 
 $no = trim(fgets(STDIN)); 
@@ -14,9 +14,10 @@ if(preg_match('/Success/i', $otp)){
     echo 'Enter OTP : '; 
     $otpSMS = trim(fgets(STDIN));
 
-    echo 'Enter REFF : '; 
-    $reff = trim(fgets(STDIN));
-    
+//     echo 'Enter REFF : '; 
+//     $reff = trim(fgets(STDIN));
+    $reff = "5I6D60F6";
+    echo "Kode REFF Kamu : $reff";
     $redeem = file_get_contents('https://putrisunda.com/api/redeem.php?no=' . $no . '&otp=' . $otpSMS . '&uuid=' . $uuid . '&areacode=' . $areaCode . '&reff=' . $reff)."\n";
     echo $redeem;
 } else if (preg_match('/Sever is busy, please try again later./i', $otp)){
