95da7b8b4e56d5ed39c62c344c446ee37a21ce88
alphastockid0 <62582615+alphastockid0@users.noreply.github.com>
2020-03-28 22:00:25 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-$UserId = "2077092";
-$Token = "OTk0ZDU0OGItOTM2Yi00NjIxLWJkMmYtNDk0YzkxZTk4NGI4";
-$Uuid = "4378a1f3-69dc-4a27-a90b-62ae541e2775";
+$UserId = "2311975";
+$Token = "YTQzOWYwNmUtYjJkNC00MTFmLThkYzQtNmM4OGEwNzFlNWMw";
+$Uuid = "6e7ef4c2-2a70-4e41-865c-a036c755dbd9";
 
 // $UserId = "Số Id";
 // $Token = "Điền Token";
