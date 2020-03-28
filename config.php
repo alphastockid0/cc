497df7f6a4a4d8babf9d68c6be588438360bafdf
alphastockid0 <62582615+alphastockid0@users.noreply.github.com>
2020-03-28 22:23:34 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
-<?
+<?php
 
-$UserId = "xxxxxx";
-$Token = "xxxxxx";
-$Uuid = "xxxxxxx";
+$UserId = "2311975";
+$Token = "YTQzOWYwNmUtYjJkNC00MTFmLThkYzQtNmM4OGEwNzFlNWMw";
+$Uuid = "6e7ef4c2-2a70-4e41-865c-a036c755dbd9";
+
+?>
